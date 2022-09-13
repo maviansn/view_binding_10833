86ebb4f9bebf0e81d99999ece3a9d34495f53d3f
maviansn <vannessatriagunawan@gmail.com>
2022-09-13 14:19:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10833/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.view_binding_10833
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.view_binding_10833.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val adapter = MainAdapter(TaskList.taskList)
+        binding?.taskRv?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
---
+++ app/src/main/java/com/example/view_binding_10833/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10833
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari primo buat gacha mama Yaemiko tercinta / Keqing wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "Dimana mama Ei", "Buat adu mekanik showcase ayaka pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat Pamer ke View Showcase Ayaka Pyro C6 + Bennet C6")
+    )
+}
